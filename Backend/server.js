92fff027360f9620aa1a1334e0d0fdfa9b3bf762
abhay-1514 <94120291+abhay-1514@users.noreply.github.com>
2024-10-01 13:16:59 +0530

    final commit

--- Backend/server.js
+++ Backend/server.js
@@ -8,7 +8,6 @@ const orderRoutes = require('./Routes/orderRoutes');
 const deliveryRoutes = require('./Routes/deliveryRoutes');
 const imageRouter = require('./Routes/imageRouter');
 require('dotenv').config({ path: './dotENV/.env' }); // Adjust the path based on the new location of your .env file
-const BASE_URL = process.env.BASE_URL;
 
 connectDB(); 
 
@@ -24,12 +23,12 @@ app.use(cors({
 app.use(express.json()); // For parsing application/json
 app.use('/uploads', imageRouter);
 // Routes
-app.use(`${BASE_URL}/api/users`, userRoutes); // User routes
-app.use(`${BASE_URL}/api/products`, productRoutes); // Product routes
-app.use(`${BASE_URL}/api/orders`, orderRoutes); // Order routes
-app.use(`${BASE_URL}/api/delivery`,deliveryRoutes);// Delivery Routes
+app.use('/api/users', userRoutes); // User routes
+app.use('/api/products', productRoutes); // Product routes
+app.use('/api/orders', orderRoutes); // Order routes
+app.use('/api/delivery',deliveryRoutes);// Delivery Routes
    
-app.use(`${BASE_URL}/api/uploads`, express.static('uploads'));
+app.use('/api/uploads', express.static('uploads'));
 
 
 // Error handling middleware
